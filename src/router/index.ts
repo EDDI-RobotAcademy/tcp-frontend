3971beaedea46ebfd7a708064c5bb7cb8f86c1ed
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-07-19 14:32:36 +0900

    feat: [한재혁] 보드 list 및 register 구현 완료 [TCPF]

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BoardRoutes from '@/board/router/BoardRoutes'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -14,7 +15,8 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  ...BoardRoutes,
 ]
 
 const router = createRouter({
