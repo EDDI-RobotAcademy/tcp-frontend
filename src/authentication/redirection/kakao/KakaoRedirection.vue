b51b0413cb07c9c2254f1db83f06e2c8a10f3ed6
Gomin <138251577+MinMessi@users.noreply.github.com>
2024-08-08 16:11:12 +0900

    feat: [민경원] 카카오 로그인 관련 AccessToken 요청과 인증을 합니다. [TCPV-8]

---
+++ src/authentication/redirection/kakao/KakaoRedirection.vue
@@ -0,0 +1,46 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+const authenticationModule = 'authenticationModule'
+const accountModule = 'accountModule'
+
+export default {
+    methods: {
+        ...mapActions(authenticationModule,
+        ['requestAccessTokenToDjangoRedirection', 'requestUserInfoToDjango', 'requestAddRedisAccessTokenToDjango']),
+        ...mapActions(accountModule, ['requestEmailDuplicationCheckToDjango']),
+
+        async setRedirectData () {
+            const code = this.$route.query.code
+            await this.requestAccessTokenToDjangoRedirection({ code })
+            const userInfo = await this.requestUserInfoToDjango()
+            const email = userInfo.kakao_account.email
+            console.log('email: ', email)
+
+            const isEmailDuplication = await this.requestEmailDuplicationCheckToDjango(email)
+            if (isEmailDuplication === true) {
+                console.log('기존 가입 고객입니다.')
+                const accessToken = localStorage.getItem("accessToken");
+                
+                if (accessToken) {
+                    await this.requestAddRedisAccessTokenToDjango({ email, accessToken });  // Fix: Pass as object directly
+                } else {
+                    console.error('AccessToken is missing');
+                }
+
+                this.$router.push('/')
+            } else {
+                console.log('신규 가입 고객입니다.')
+                this.$router.push('/account/register')
+            }
+        }
+    },
+    async created () {
+        await this.setRedirectData()
+    }
+}
+</script>
