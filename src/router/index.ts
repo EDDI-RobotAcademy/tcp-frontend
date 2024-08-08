2fe7b5c5799e811dd6db5d3d1a19ca9f999b2371
Gomin <138251577+MinMessi@users.noreply.github.com>
2024-08-08 15:39:13 +0900

    fix: [민경원] board 도메인 삭제 [TCPVx]

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,14 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import BoardRoutes from '@/board/router/BoardRoutes'
+import HomeRoutes from '@/home/router/HomeRoutes'
+import AccountRoutes from '@/account/router/AccountRoutes'
+import AuthenticationRoutes from '@/authentication/router/AuthenticationRoutes'
+import CommunityRoutes from '@/community/router/CommunityRoutes'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  ...BoardRoutes,
+  ...HomeRoutes,
+  ...AccountRoutes,
+  ...AuthenticationRoutes,
+  ...CommunityRoutes,
 ]
 
 const router = createRouter({
